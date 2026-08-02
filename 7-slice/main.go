626a515fc2c281b196f6a644eb2206e8e9ee6d1d
jonas <liam.tech09@gmail.com>
2026-08-02 23:50:45 +0800

    slice

--- 7-slice/main.go
+++ 7-slice/main.go
@@ -2,13 +2,11 @@ package main
 
 import (
 	"fmt"
-	
 )
 
-func main () {
-	slice := []int{1, 2, 3, 4, 5}
-	fmt.Println(slice)
-	fmt.Println(slice[1:3])
-	fmt.Println(slice[1:])
-	fmt.Println(slice[:3])
+func main() {
+	s := []int{1, 2, 3, 4, 5}
+	fmt.Println(s)
+	testSlice(s) 
+	fmt.Println(s)
 }
